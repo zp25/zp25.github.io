d581e8a7d015f0d1b4934020a8b286436148a3aa
zp25 <zebrap25@gmail.com>
2016-09-10 14:44:00 +0800

    New: service worker

---
+++ sw.js
@@ -0,0 +1,60 @@
+/** @type {Number} Cache版本 */
+const CACHE_VERSION = 0.1;
+
+/** @type {Object} 当前可用cacheName */
+const CURRENT_CACHES = {
+  offline: `offline-v${CACHE_VERSION}`,
+};
+
+/** @type {Array}  */
+const OFFLINE_URL = [
+  '404.html'
+];
+
+/** 安装，预先获取需缓存资源 */
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CURRENT_CACHES.offline).then(cache => cache.addAll(OFFLINE_URL))
+  );
+});
+
+/** 清理 */
+self.addEventListener('activate', event => {
+  const cacheWhitelist = Object.values(CURRENT_CACHES);
+
+  event.waitUntil(
+    caches.keys().then(keyList => Promise.all(keyList.map(key => {
+      if (!cacheWhitelist.includes(key)) {
+        return caches.delete(key);
+      }
+
+      return true;
+    })))
+  );
+});
+
+/** 代理，部分浏览器暂不支持navigation request */
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(res => {
+      if (res) {
+        return res;
+      }
+
+      return fetch(event.request).then(res => {
+        if (!res.ok) {
+          return new Response('Response Error', {
+            status: res.status,
+            statusText: res.statusText,
+          });
+        }
+
+        return caches.open(CURRENT_CACHES.offline).then(cache => {
+          cache.put(event.request, res.clone());
+
+          return res;
+        })
+      }).catch(() => caches.match('404.html'));
+    })
+  )
+});
