4208e6f0dd806deb92a75711dca6c521bc4dc784
zp25 <zebrap25@gmail.com>
2016-09-14 11:08:26 +0800

    Fix: ios点击链接会使用Safari打开

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,29 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('../sw.js', { scope: '/' });
 }
+
+/**
+ * 导航事件处理
+ * @param {Event} e 事件对象
+ */
+function triggerNavigator(e) {
+  e.preventDefault();
+
+  window.location = e.target.href;
+}
+
+/**
+ * 事件捕获和分发
+ * @param {Event} e 事件对象
+ */
+function eventHandler(e) {
+  const trigger = e.target.dataset.trigger;
+
+  if (trigger === 'navigator') {
+    triggerNavigator(e);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.body.addEventListener('click', eventHandler);
+});
