acb8cbd180b806d4ed2dd66c8365690ab1fd16a2
zp25 <zebrap25@gmail.com>
2017-04-19 14:40:46 +0800

    refactor: js重写，sw状态反馈调整

--- scripts/main.js
+++ scripts/main.js
@@ -1,68 +1,119 @@
 /**
- * service worker状态变更处理
- * @param {Event} e 事件对象
+ * service worker管理
+ * @return {Object}
  */
-function swStateChange(e) {
-  const t = document.querySelector('.sw .sw__state');
-  const state = e.target.state;
+const swControl = () => {
+  const target = document.querySelector('.sw');
 
-  t.classList.remove('sw__state--ready', 'sw__state--error');
+  /**
+   * 反馈状态的样式修改
+   * @param {string} state 当前状态
+   */
+  const style = (state) => {
+    target.classList.remove('sw--waiting', 'sw--error', 'sw--done', 'sw--no-support');
 
-  if (state === 'activated') {
-    t.classList.add('sw__state--ready');
-    t.innerHTML = 'activated';
+    if (state === 'waiting') {
+      target.classList.add('sw--waiting');
+    } else if (state === 'error') {
+      target.classList.add('sw--error');
+    } else if (state === 'done') {
+      target.classList.add('sw--done');
+    } else if (state === 'no-support') {
+      target.classList.add('sw--no-support');
+    }
+  };
+
+  /**
+   * 状态变更处理
+   * @param {Event} e - 事件对象
+   */
+  const stateChange = (e) => {
+    const state = e.target.state;
+
+    if (state === 'activated') {
+      style('done');
+    } else {
+      style('waiting');
+    }
+  };
+
+  return {
+    style,
+    stateChange,
+  };
+};
+
+const sw = () => {
+  const control = swControl();
+
+  if (typeof navigator.serviceWorker === 'undefined') {
+    control.style('no-support');
+    return;
   }
-}
 
-if ('serviceWorker' in navigator) {
   const serviceWorker = navigator.serviceWorker;
-  const t = document.querySelector('.sw .sw__state');
 
   serviceWorker.register('/sw.js', { scope: '/' }).then((reg) => {
-    const sw = reg.installing || reg.waiting || reg.active;
-
-    // 直接进入activated状态
-    if (serviceWorker.controller) {
-      t.classList.add('sw__state--ready');
-      t.innerHTML = 'activated';
+    if (reg.installing || reg.waiting) {
+      control.style('waiting');
+    } else if (reg.active) {
+      // activating, activated状态
+      control.style('done');
     }
 
-    sw && sw.addEventListener('statechange', swStateChange);
+    if (serviceWorker) {
+      serviceWorker.addEventListener('statechange', control.stateChange);
+    }
   }).catch((err) => {
-    t.classList.remove('sw__state--ready');
-    t.classList.add('sw__state--error')
-
-    t.innerHTML = err.message;
+    control.style('error');
+    console.log(err.message); // eslint-disable-line no-console
   });
-} else {
-  document.querySelector('.sw .sw__state').innerHTML = 'No support';
 }
 
 /**
- * 导航事件处理
- * @param {Event} e 事件对象
+ * 事件分发
  */
-function triggerNavigator(e) {
-  if (navigator.standalone) {
-    e.preventDefault();
-
-    location.href = e.target.href;
-  }
-}
-
-/**
- * 事件捕获和分发
- * @param {Event} e 事件对象
- */
-function eventHandler(e) {
+const dispatch = handlers => (e) => {
   const trigger = e.target.dataset.trigger;
 
-  if (trigger === 'navigator') {
-    triggerNavigator(e);
+  if (trigger && {}.hasOwnProperty.call(handlers, trigger)) {
+    handlers[trigger](e);
   }
-}
+};
+
+const createHandlers = () => {
+  /**
+   * 导航事件处理
+   * @param {Event} e - 事件对象
+   */
+  const navigator = (e) => {
+    if (navigator.standalone) {
+      e.preventDefault();
+
+      location.href = e.target.href;
+    }
+  };
+
+  /**
+   * 阻止默认动作
+   * @param {Event} e - 事件对象
+   */
+  const prevent = (e) => {
+    e.preventDefault();
+  };
+
+  return {
+    navigator,
+    prevent,
+  };
+};
 
 document.addEventListener('DOMContentLoaded', () => {
+  const handlers = createHandlers();
+
+  // 启动service worker
+  sw();
+
   // 捕获全局点击事件
-  document.body.addEventListener('click', eventHandler, false);
+  document.body.addEventListener('click', dispatch(handlers), false);
 });
